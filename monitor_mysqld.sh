630e74f4926919cd9b4f8cdebcb4f1cbefd34557
Jakofff <jakofff@users.noreply.github.com>
2016-09-11 13:06:10 -0500

    Update monitor_mysqld.sh

--- monitor_mysqld.sh
+++ monitor_mysqld.sh
@@ -2,7 +2,8 @@
 STATUS=$(systemctl status mysqld | grep running | grep -v not | wc -l);
 if [ "$STATUS" -ne 1 ];
 then
-        echo "MySQL not running";
+        echo $(date) "- MySQL not running\n";
         systemctl stop mysqld
         systemctl start mysqld
 fi
+
